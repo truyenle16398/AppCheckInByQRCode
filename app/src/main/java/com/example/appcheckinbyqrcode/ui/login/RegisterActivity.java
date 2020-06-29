7e4bc3c86c271007813eccb580b7accc7809f7dc
kietnguyen711998 <37497153+kietnguyen711998@users.noreply.github.com>
2020-06-29 11:09:25 +0700

    Revert "Kiet nguyen"

--- app/src/main/java/com/example/appcheckinbyqrcode/ui/login/RegisterActivity.java
+++ app/src/main/java/com/example/appcheckinbyqrcode/ui/login/RegisterActivity.java
@@ -5,8 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -112,33 +110,6 @@ public class RegisterActivity extends AppCompatActivity {
 
             }
         });
-        edtemail.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-            @Override
-            public void afterTextChanged(Editable s) {
-                email = edtemail.getText().toString().trim();
-                if (isEmailValid(email))
-                {
-                    edtemail.setError(null);
-                }
-                else
-                {
-                    if (email.isEmpty()){
-                        edtemail.setError("Vui lòng nhập trường này");
-                    } else {
-                        edtemail.setError("Email sai định dạng");
-                    }
-                }
-            }
-        });
     }
     private boolean validateEmail(String email) {
         String emails = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
@@ -160,25 +131,5 @@ public class RegisterActivity extends AppCompatActivity {
         edtconfirmpass = findViewById(R.id.inputPassConfirm);
         TVBack = findViewById(R.id.tvBack);
         btnregister = findViewById(R.id.btnRegister);
-    }
-    public boolean isEmailValid(String email)
-    {
-        String regExpn =
-                "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]{1}|[\\w-]{2,}))@"
-                        +"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
-                        +"[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
-                        +"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
-                        +"[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
-                        +"([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$";
-
-        CharSequence inputStr = email;
-
-        Pattern pattern = Pattern.compile(regExpn,Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(inputStr);
-
-        if(matcher.matches())
-            return true;
-        else
-            return false;
     }
 }
